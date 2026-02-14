7a8c3eab0d82216efb393256ca2eb21985ff1be7
AL ERSHAD BHUIYAN <ebhuiyan35@gmail.com>
2026-02-14 08:09:11 +0600

    Add files via upload

---
+++ style/js/scripts.js
@@ -0,0 +1,258 @@
+$(document).ready(function() {
+    'use strict';
+    /*-----------------------------------------------------------------------------------*/
+    /*	STICKY HEADER
+    /*-----------------------------------------------------------------------------------*/
+    var options = {
+        offset: 350,
+        offsetSide: 'top',
+        classes: {
+            clone: 'banner--clone fixed',
+            stick: 'banner--stick',
+            unstick: 'banner--unstick'
+        },
+        onStick: function() {
+            $($.SmartMenus.Bootstrap.init);
+        }
+    };
+    var banner = new Headhesive('.navbar', options);
+    $('.offset').css('padding-top', $('.navbar').height() + 'px');
+    $(window).resize(function() {
+        $('.offset').css('padding-top', $('.navbar').height() + 'px');
+    });
+    $('.onepage .navbar .nav li a').on('click', function() {
+        $('.navbar .navbar-collapse.in').collapse('hide');
+        $('.nav-bars').removeClass('is-active');
+    });
+    /*-----------------------------------------------------------------------------------*/
+    /*	HAMBURGER MENU ICON
+    /*-----------------------------------------------------------------------------------*/
+    $(".nav-bars").on( "click", function() {
+        $(".nav-bars").toggleClass("is-active");
+    });
+    /*-----------------------------------------------------------------------------------*/
+    /*	CUBE PORTFOLIO
+    /*-----------------------------------------------------------------------------------*/
+    $('#js-grid-slider-testimonials').cubeportfolio({
+        layoutMode: 'slider',
+        drag: true,
+        auto: false,
+        autoTimeout: 5000,
+        autoPauseOnHover: true,
+        showNavigation: false,
+        showPagination: true,
+        rewindNav: true,
+        scrollByPage: false,
+        gridAdjustment: 'responsive',
+        mediaQueries: [{
+            width: 1,
+            cols: 1
+        }],
+        gapHorizontal: 0,
+        gapVertical: 0,
+        caption: '',
+        displayType: 'default',
+    });
+    $('#clients').cubeportfolio({
+        layoutMode: 'slider',
+        drag: true,
+        auto: false,
+        autoTimeout: 5000,
+        autoPauseOnHover: true,
+        showNavigation: false,
+        showPagination: false,
+        rewindNav: false,
+        scrollByPage: false,
+        gridAdjustment: 'responsive',
+        mediaQueries: [{
+            width: 800,
+            cols: 6
+        }, {
+            width: 480,
+            cols: 4
+        }, {
+            width: 320,
+            cols: 2
+        }],
+        gapHorizontal: 0,
+        gapVertical: 50,
+        caption: 'overlayBottom',
+        displayType: 'fadeIn',
+        displayTypeSpeed: 100,
+
+    });
+    /*-----------------------------------------------------------------------------------*/
+    /*	LIGHTGALLERY
+	/*-----------------------------------------------------------------------------------*/
+    $('.light-gallery').lightGallery({
+        thumbnail: false,
+        selector: '.lgitem',
+        animateThumb: true,
+        showThumbByDefault: false,
+        download: false,
+        autoplayControls: false,
+        zoom: false,
+	    fullScreen: false,
+        thumbWidth: 100,
+        thumbContHeight: 80,
+        videoMaxWidth: '1000px'
+    });
+    /*-----------------------------------------------------------------------------------*/
+    /*	GO TO TOP
+    /*-----------------------------------------------------------------------------------*/
+    $.scrollUp({
+        scrollName: 'scrollUp',
+        // Element ID
+        scrollDistance: 300,
+        // Distance from top/bottom before showing element (px)
+        scrollFrom: 'top',
+        // 'top' or 'bottom'
+        scrollSpeed: 300,
+        // Speed back to top (ms)
+        easingType: 'linear',
+        // Scroll to top easing (see http://easings.net/)
+        animation: 'fade',
+        // Fade, slide, none
+        animationInSpeed: 200,
+        // Animation in speed (ms)
+        animationOutSpeed: 200,
+        // Animation out speed (ms)
+        scrollText: '<span class="btn btn-square"><i class="ion-android-arrow-up"></i></span>',
+        // Text for element, can contain HTML
+        scrollTitle: false,
+        // Set a custom <a> title if required. Defaults to scrollText
+        scrollImg: false,
+        // Set true to use image
+        activeOverlay: false,
+        // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+        zIndex: 1001 // Z-Index for the overlay
+    });
+    /*-----------------------------------------------------------------------------------*/
+    /*	IMAGE ICON HOVER
+    /*-----------------------------------------------------------------------------------*/
+    $('.overlay a').prepend('<span class="over"><span></span></span>');
+    /*-----------------------------------------------------------------------------------*/
+    /*	TAB COLLAPSE
+    /*-----------------------------------------------------------------------------------*/
+    $('#tab1').tabCollapse({
+        tabsClass: 'hidden-sm hidden-xs',
+        accordionClass: 'visible-sm visible-xs'
+    });
+    $('#tab1').on('shown-accordion.bs.tabcollapse', function() {
+        $('.panel-group').find('.panel-default:has(".in")').addClass('panel-active');
+        $('.panel-group').on('shown.bs.collapse', function(e) {
+            $(e.target).closest('.panel-default').addClass(' panel-active');
+        }).on('hidden.bs.collapse', function(e) {
+            $(e.target).closest('.panel-default').removeClass(' panel-active');
+        });
+    });
+    /*-----------------------------------------------------------------------------------*/
+    /*	TOGGLE
+    /*-----------------------------------------------------------------------------------*/
+    $('.panel-group').find('.panel-default:has(".in")').addClass('panel-active');
+    $('.panel-group').on('shown.bs.collapse', function(e) {
+        $(e.target).closest('.panel-default').addClass(' panel-active');
+    }).on('hidden.bs.collapse', function(e) {
+        $(e.target).closest('.panel-default').removeClass(' panel-active');
+    });
+    /*-----------------------------------------------------------------------------------*/
+    /*	PROGRESS BAR
+    /*-----------------------------------------------------------------------------------*/
+    $('.progress-list .progress .bar').progressBar({
+        shadow: false,
+        percentage: false,
+        animation: true,
+        height: 12
+    });
+    /*-----------------------------------------------------------------------------------*/
+    /*	DATA REL
+    /*-----------------------------------------------------------------------------------*/
+    $('a[data-rel]').each(function() {
+        $(this).attr('rel', $(this).data('rel'));
+    });
+    /*-----------------------------------------------------------------------------------*/
+    /*	TOOLTIP
+    /*-----------------------------------------------------------------------------------*/
+    if ($("[rel=tooltip]").length) {
+        $("[rel=tooltip]").tooltip();
+    }
+    /*-----------------------------------------------------------------------------------*/
+    /*	PRETTIFY
+    /*-----------------------------------------------------------------------------------*/
+    window.prettyPrint && prettyPrint();
+    /*-----------------------------------------------------------------------------------*/
+    /*	PARALLAX MOBILE
+    /*-----------------------------------------------------------------------------------*/
+    if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i)) {
+        $('.parallax').addClass('mobile');
+    }
+    /*-----------------------------------------------------------------------------------*/
+    /*	BACKGROUND VIDEO PARALLAX
+    /*-----------------------------------------------------------------------------------*/
+    $('#video-fish').backgroundVideo({
+        $outerWrap: $('.outer-wrap'),
+        pauseVideoOnViewLoss: false,
+        parallaxOptions: {
+            effect: 1.9
+        }
+    });
+    /*-----------------------------------------------------------------------------------*/
+    /*	LOADER
+    /*-----------------------------------------------------------------------------------*/
+    $(".pageloader").lsPreloader({
+
+        backgroundColor: "#252525",
+        logoImage: "style/images/logo-light.png",
+        minimumTime: .5,
+        progressHeight: "0",
+        progressColor: "#fff",
+        percentFontSize: "18px"
+
+    });
+    /*-----------------------------------------------------------------------------------*/
+    /*	COUNTER UP
+    /*-----------------------------------------------------------------------------------*/
+    $('.fcounter').counterUp({
+        delay: 50,
+        time: 1000
+    });
+    /*-----------------------------------------------------------------------------------*/
+    /*	COLLAGEPLUS
+    /*-----------------------------------------------------------------------------------*/
+    collage();
+
+
+    function collage() {
+        $('#collage-large').removeWhitespace().collagePlus({
+            'fadeSpeed': 5000,
+            'targetHeight': 400,
+            'effect': 'effect-2',
+            'direction': 'vertical',
+            'allowPartialLastRow': true
+        });
+        $('#collage-medium').removeWhitespace().collagePlus({
+            'fadeSpeed': 5000,
+            'targetHeight': 300,
+            'effect': 'effect-2',
+            'direction': 'vertical',
+            'allowPartialLastRow': true
+        });
+    };
+    
+    
+    
+    // This is just for the case that the browser window is resized
+    var resizeTimer = null;
+    $(window).on('resize', function() {
+        // hide all the images until we resize them
+        $('.collage .collage-image-wrapper').css("opacity", 0);
+        // set a timer to re-apply the plugin
+        if (resizeTimer) clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(collage, 200);
+    });
+    /*-----------------------------------------------------------------------------------*/
+    /*	VANILLA
+    /*-----------------------------------------------------------------------------------*/
+    var myForm;
+    myForm = new VanillaForm($("form.vanilla-form"));
+});
